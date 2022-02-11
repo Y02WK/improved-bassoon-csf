0fbe347d71590b9795f7f420fe8d5912bdb79aa9
Y02WK <59220086+Y02WK@users.noreply.github.com>
2022-02-11 13:58:23 +0800

    Added form

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
@@ -31,6 +35,11 @@ const appRoutes: Routes = [
     HttpClientModule,
     MatCardModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [RecipeService],
   bootstrap: [AppComponent],
--- client/src/app/components/recipe-add/recipe-add.component.ts
+++ client/src/app/components/recipe-add/recipe-add.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-add',
   templateUrl: './recipe-add.component.html',
-  styleUrls: ['./recipe-add.component.css']
+  styleUrls: ['./recipe-add.component.css'],
 })
 export class RecipeAddComponent implements OnInit {
-
-  constructor() { }
+  form!: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      title: this.fb.control('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      ingredients: this.fb.array([
+        new FormControl('', [Validators.required, Validators.minLength(3)]),
+      ]),
+    });
   }
 
+  addInput() {
+    const control = new FormControl('');
+    (<FormArray>this.form.get('ingredients')).push(control);
+  }
+
+  getFormArray(): FormArray {
+    return this.form.get('ingredients') as FormArray;
+  }
 }
